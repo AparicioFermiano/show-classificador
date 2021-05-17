7d6ca165f48027a7e8be9c0a273c7a74ab03dd12
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-05-17 13:21:59 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+const idadePrioritaria = 60;
+
+const funcoes = {
+  ClienteCriado: (cliente) => {
+    cliente.status =
+      cliente.idade >= idadePrioritaria ? "Prioritario" : "Comum";
+    axios.post("http://localhost:10000/eventos"),
+      {
+        tipo: "ClienteCriado",
+        dados: cliente,
+      };
+  },
+};
+
+app.post("/eventos", (req, res) => {
+  funcoes[req.body.tipo](req.body.dados);
+  res.status(200).send({ msg: "ok" });
+});
+
+app.post("/eventos", (req, res) => {});
+
+app.listen(port, () => console.log("classificacao, Porta 4000"));
+
